622a90b3fd2c55b824674a6a126fafd11953f209
kmidtlid3@gmail.com <kmidtlid3@gmail.com>
2020-02-19 14:51:54 +0100

    Cleaned

--- project1/main.py
+++ project1/main.py
@@ -12,8 +12,6 @@ actor = Actor()
 # Keep track of pegs left on board for each episode (for plotting)
 remaining_pegs = []
 
-epsilons = []
-
 # Local variable for the epsilon greedy strategy
 epsilon = epsilon_greedy_value
 
@@ -25,34 +23,34 @@ for episode in range(num_of_episodes):
   critic.reset_eligibilities()
   actor.reset_eligibilities()
 
+  # Get initial state
   state, legal_moves, reward, done = env.get_current_state()
 
   if critic_type == "table":
     critic.add_state(state)
     
+  # Get initial action
   action = actor.choose_action(state, legal_moves, epsilon)
 
-  saps = [(state, action, reward)] # All state-action pairs for this episode
+  saps = [(state, action, reward)] # Will contain all state-action pairs for this episode
 
-  step = 0
   while done == 0: # If done is -1 the game is lost, if done is 1 the game is won
-    step += 1
+    # Do action
     new_state, legal_moves, reward, done = env.move(action)
 
     if critic_type == "table":
       critic.add_state(new_state)
 
+    # Choose new action
     new_action = actor.choose_action(new_state, legal_moves, epsilon)
 
     actor.update_eligibility(state, action, 1)
-
 
     if critic_type == "table":
       critic.update_delta(reward, new_state, state)
       critic.update_eligibility(state, 1)
 
-
-    # if len(legal_moves) > 0: # The new state is not terminal, so we add it to saps
+    # Add new state, action and reward to the state-action pairs for this episode
     saps.append((new_state, new_action, reward))
 
     if critic_type == "table":
@@ -63,42 +61,30 @@ for episode in range(num_of_episodes):
         actor.update_policy(s, a, critic.delta)
         actor.update_eligibility(s, a)
     else:
-      # Evaluate the second to last state, need the successor state as well
+      # Evaluate all states and get their corresponding td error
       td_errors = critic.nn_update_eval(saps)
       for i in range(len(saps)-1):
         s, a, r = saps[i]
-        # if i == len(saps)-1:
-        #   if reward > 0:
-        #     actor.update_policy(s, a, 10)
-        #   else:
-        #     actor.update_policy(s, a, 0)
-        # else:
-        #   actor.update_policy(s, a, td_errors[i])
         actor.update_policy(s, a, td_errors[i])
         actor.update_eligibility(s, a)
 
     action = new_action
     state = new_state
 
-    #epsilon = max(0, epsilon_greedy_value-(episode/(num_of_episodes*0.5))) # Reduces the epsilon, but never below 0
-    # if episode+1 == num_of_episodes - 100:
-    #   epsilon = 0
-    # else:
+    # Decay epsilon for the epsilon greedy strategy
     epsilon = epsilon * epsilon_decay_rate
 
   print("Episode:", episode + 1)
-  print("New delta:", critic.delta)
 
   if (env.get_remaining_pegs() == 1):
     print("Solved!")
 
-  remaining_pegs.append(env.get_remaining_pegs())
-  epsilons.append(epsilon)
+  remaining_pegs.append(env.get_remaining_pegs()) # For plotting
 
   if episode == num_of_episodes-1: # Visualize the last episode
     env.show()
 
-plot_remaining_pegs(remaining_pegs, epsilons)
+plot_remaining_pegs(remaining_pegs)
 
 if critic_type == "neural_net":
   critic.value_func.summary()
